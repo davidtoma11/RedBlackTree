5d8a6ddedc426cc2459e49bc42e1e402fde74a2c
Toma David <120136460+davidtoma11@users.noreply.github.com>
2023-06-03 21:31:28 +0300

    Update Structuri de date avansata.py

--- Structuri de date avansata.py
+++ Structuri de date avansata.py
@@ -14,7 +14,7 @@ class Node:
         self.left = None        # Subarborele stang al nodului (initial gol)
         self.right = None       # Subarborele drept al nodului (initial gol)
         self.parent = None      # Parintele nodului (initial gol)
-        self.color = 1          # Culoarea nodului (0 = negru, 1 = roșu)
+        self.color = 0          # Culoarea nodului (0 = negru, 1 = roșu)
 
 # Definim clasa pentru RedBlackTree
 class RedBlackTree:
@@ -24,112 +24,126 @@ class RedBlackTree:
         self.nil.color = 0      # Nodul nil este negru
         self.root = self.nil    # Initial, arborele nu are niciun nod, deci rădăcina este nil
 
-    # Functia de inserare a unui nod în arbore
     def insert(self, key):
-        # Cream un nou nod
+        # Cream un nou nod cu cheia specificată
         new_node = Node(key)
 
-        # Initial, noul nod este adaugat la sfarsitul arborelui (ca frunza)
+        # Adaugam nodul ca o frunza
         new_node.left = self.nil
         new_node.right = self.nil
 
-        # Incercam să gasim locul unde sa inseram noul nod
-        current = self.root      # Pornim de la radacina arborelui
-        parent = None            # Parintele curent initial este nul
+        current = self.root  # Pornim de la rădăcina arborelui
+        parent = None
+
         while current != self.nil:
-            parent = current     # Nodul curent devine parintele pentru urmatorul pas
+            parent = current  # Nodul curent devine parintele pentru urmatorul pas
+
+            # Comparăm cheia noului nod cu cheia nodului curent
             if new_node.key < current.key:
                 current = current.left
             else:
                 current = current.right
-        # Am găsit locul unde să inserăm noul nod, deci îl adăugăm ca fiu al părintelui găsit
-        new_node.parent = parent
-        if parent == None:
+
+
+        new_node.parent = parent  # Conectam noul nod ca fiu al parintelui gasit
+        if parent is None:
+            # Daca parintele este None, inseamna că arborele era inițial gol și noul nod devine radacina arborelui
             self.root = new_node
         elif new_node.key < parent.key:
             parent.left = new_node
         else:
             parent.right = new_node
-        # Setăm culoarea noului nod la roșu
+
+        # Setam culoarea noului nod la rosu
         new_node.color = 1
-        # Aplicăm regulile de red-black tree pentru a menține proprietățile acestuia
 
-        #self.insert_fixup(new_node)
+        # "Reparam" arborele
+        self.insert_fixup(new_node)
 
-    # Functia pentru mentinerea proprietatilor RBT, dupa inserarea unui nod
+
     def insert_fixup(self, node):
-        # Cat timp parintele si bunicul nodului nou inserat sunt rosii, trebuie sa aplicam rotatii si schimbari de culori
-        if node.parent is not None:
-            while node.parent.color == 1 and node.parent != self.root:
-                # Daca parintele este fiul stang al bunicului
-                if node.parent == node.parent.parent.left:
-                    uncle = node.parent.parent.right
-                    # Daca unchiul este rosu, schimbam culorile pentru parinte, unchi si bunic, apoi ne mutam in sus in arbore
-                    if uncle.color == 1:
-                        node.parent.color = 0
-                        uncle.color = 0
-                        node.parent.parent.color = 1
-                        node = node.parent.parent
-                    else:
-                        # Daca nodul este fiul drept al parintelui, trebuie sa aplicam o rotatie la stanga, astfel incat
-                        # parintele sa devina noul nod radacina pentru aceasta subarbore
-                        if node == node.parent.right:
-                            node = node.parent
-                            self.left_rotate(node)
-                        # Schimbam culorile pentru parinte si bunic, apoi aplicam o rotatie la dreapta pentru a creste subarborele
-                        # spre varful arborelui si pentru a pastra proprietatile Red-Black
-                        node.parent.color = 0
-                        node.parent.parent.color = 1
-                        self.right_rotate(node.parent.parent)
-                # Daca parintele este fiul drept al bunicului, procedam simetric
+        while node.parent is not None and node.parent.color == 1:  # Verificăm dacă părintele nodului este roșu
+            if node.parent == node.parent.parent.left:  # Cazul în care părintele nodului este fiul stâng al bunicului său
+                uncle = node.parent.parent.right  # Obținem unchiul nodului (fratele părintelui)
+                if uncle.color == 1:  # Cazul 1: Unchiul nodului este, de asemenea, roșu
+                    node.parent.color = 0
+                    uncle.color = 0
+                    node.parent.parent.color = 1
+                    node = node.parent.parent
                 else:
-                    uncle = node.parent.parent.left
-                    if uncle.color == 1:
-                        node.parent.color = 0
-                        uncle.color = 0
-                        node.parent.parent.color = 1
-                        node = node.parent.parent
-                    else:
-                        if node == node.parent.left:
-                            node = node.parent
-                            self.right_rotate(node)
-                        node.parent.color = 0
-                        node.parent.parent.color = 1
-                        self.left_rotate(node.parent.parent)
+                    if node == node.parent.right:  # Cazul 2: Unchiul nodului este negru și nodul este fiul drept al părintelui
+                        node = node.parent  # Reducem cazul 2 la cazul 3 prin rotire la stânga pe părinte
+                        self.left_rotate(node)
+                    # Cazul 3: Unchiul nodului este negru și nodul este fiul stâng al părintelui
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    self.right_rotate(node.parent.parent)
+            else:  # Cazul în care parintele nodului este fiul drept al bunicului sau
+                uncle = node.parent.parent.left
+                if uncle.color == 1:  # Cazul 1: Unchiul nodului este, de asemenea, roșu
+                    node.parent.color = 0
+                    uncle.color = 0
+                    node.parent.parent.color = 1
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:  # Cazul 2: Unchiul nodului este negru și nodul este fiul stâng al părintelui
+                        node = node.parent  # Reducem cazul 2 la cazul 3 prin rotire la dreapta pe părinte
+                        self.right_rotate(node)
+                    # Cazul 3: Unchiul nodului este negru și nodul este fiul drept al părintelui
+                    node.parent.color = 0
+                    node.parent.parent.color = 1
+                    self.left_rotate(node.parent.parent)
 
-        # Setam radacina arborelui la negru, pentru a indeplini regula ca radacina trebuie sa fie neagra
-        self.root.color = 0
+        self.root.color = 0  # Setăm culoarea rădăcinii la negru pentru a respecta proprietățile Red-Black Tree
 
-    # Functia de rotire la dreapta a unui subarbore cu rădăcina în nodul x
+
     def right_rotate(self, x):
+        # Salvam copilul stanga al nodului x în variabila y
         y = x.left
+
+        # Actualizam legatura dintre x și copilul dreapta al lui y
         x.left = y.right
         if y.right != self.nil:
             y.right.parent = x
+
+        # Actualizăm legăturile dintre y și părinții lui x
         y.parent = x.parent
-        if x.parent == None:
+        if x.parent is None:
             self.root = y
         elif x == x.parent.right:
             x.parent.right = y
         else:
             x.parent.left = y
-        y.right = x  # setăm x ca fiu drept al lui y
-        x.parent = y  # setăm y ca părinte al lui x
 
-    # Functia de rotire la stânga a unui subarbore cu radacina în nodul x
+        # Setam x ca fiu drept al lui y
+        y.right = x
+
+        # Setam y ca părinte al lui x
+        x.parent = y
+
+
     def left_rotate(self, x):
+        # Salvam copilul dreapta al nodului x în variabila y
         y = x.right
+
+        # Actualizăm legatura dintre x și copilul stanga al lui y
         x.right = y.left
         if y.left != self.nil:
             y.left.parent = x
+
+        # Actualizam legăturile dintre y și părinții lui x
         y.parent = x.parent
-        if x.parent == None:
+        if x.parent is None:
             self.root = y
         elif x == x.parent.left:
             x.parent.left = y
         else:
             x.parent.right = y
+
+        # Setam x ca fiu stâng al lui y
         y.left = x
+
+        # Setam y ca părinte al lui x
         x.parent = y
 
     # Functia de cautare a unui nod x
@@ -158,16 +172,12 @@ class RedBlackTree:
             u.parent.right = v
         v.parent = u.parent
 
-    # Functia de stergere a unui nod x
     def delete(self, key):
-        # Cautam nodul cu cheia specificata si il stocam în variabila 'node'
-        node = self.search(key)
 
-        # Daca nu am gasit nodul, iesim din functie
+        node = self.search(key)
         if node == self.nil:
             return
 
-        # Salvam culoarea nodului inainte de a-l șterge
         original_color = node.color
 
         # Daca nodul are doar un subarbore, il eliminam si ii legam parintelui subarborele corespunzator
@@ -193,15 +203,14 @@ class RedBlackTree:
                 successor.right.parent = successor
 
             self.transplant(node, successor)  # Inlocuim nodul cu succesorul
-            successor.left = node.left  # Succesorul primeste subarborele stang al nodului de sters
+            successor.left = node.left
             successor.left.parent = successor
-            successor.color = node.color  # Succesorul primeste culoarea originala a nodului de sters
+            successor.color = node.color
 
         # Daca nodul sters era negru, trebuie sa ne asiguram ca regulile de RedBlackTree sunt respectate
         if original_color == 0:
             self.delete_fixup(child)
 
-    # Functia pentru mentinerea proprietatilor RBT, dupa stergerea unui nod
     def delete_fixup(self, node):
         # Executam fixup pana cand nodul devine radacina sau culoarea lui este rosie
         while node != self.root and node.color == 0:
@@ -265,12 +274,27 @@ class RedBlackTree:
         if node == self.nil:
             return
         self.show(node.left)
-        print(node.key,end= "")
+        print(node.key, end="-")
         if node.color == 0:
             print("B")
-        elif node.color == 1:
+        else:
             print("R")
         self.show(node.right)
+
+
+rb_tree = RedBlackTree()
+
+rb_tree.insert(50)
+rb_tree.insert(30)
+rb_tree.insert(70)
+rb_tree.insert(20)
+rb_tree.insert(40)
+rb_tree.insert(60)
+rb_tree.insert(80)
+
+
+print("Arborele Red-Black dupa inserare:")
+rb_tree.show(rb_tree.root)
 
 
 
